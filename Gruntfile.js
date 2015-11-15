6f13f4b846daba792fc19b62d4d5e4675ddff85e
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-11-15 19:11:49 +0100

    * new dist file * added grunt * added package.json

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            js: {
+                files : {
+                    'dist/aping-plugin-youtube.min.js' : [
+                        'src/aping-youtube-directive.js',
+                        'src/aping-youtube-helper.js'
+                    ]
+                }
+            },
+            options: {
+                banner: '\n/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+            }
+        },
+        watch: {
+            minifiyJs: {
+                files: [
+                    'src/aping-youtube-directive.js',
+                    'src/aping-youtube-helper.js'
+                ],
+                tasks: ['uglify'],
+                options: {
+                    spawn: true,
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+
+};
+
