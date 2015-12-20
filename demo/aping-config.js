d03047bcf68f995616c195ab65cb0ba9ba32c60b
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-20 17:40:07 +0100

    refac: small change

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -3,8 +3,7 @@ apingApp.config(['$provide', function ($provide) {
 
     $provide.constant("apingApiKeys", {
         youtube : [
-            //{'apiKey':'<YOUR_YOUTUBE_API_KEY>'},
-            {'apiKey':'AIzaSyARYVuV6dho71EMZI6j6-sDEgo8OOnFygM'},
+            {'apiKey':'<YOUR_YOUTUBE_API_KEY>'},
         ]
     });
 
