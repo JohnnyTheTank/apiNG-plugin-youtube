05b0bc4e4a96009e8681ef1098ef5660c3f3f201
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-23 23:46:04 +0100

    config: updated to new aping strukture

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -1,10 +1,11 @@
 "use strict";
-apingApp.config(['$provide', function ($provide) {
+angular.module('jtt_aping').config(['$provide', function ($provide) {
     $provide.value("apingDefaultSettings", {
         apingApiKeys : {
             //...
             youtube : [
-                {'apiKey':'<YOUR_YOUTUBE_API_KEY>'},
+                //{'apiKey':'<YOUR_YOUTUBE_API_KEY>'},
+                {'apiKey':'AIzaSyARYVuV6dho71EMZI6j6-sDEgo8OOnFygM'},
             ]
             //...
         }
